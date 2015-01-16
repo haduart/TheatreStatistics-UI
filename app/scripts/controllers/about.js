300d8ede593e0546c3462d22ff4f842c969382f9
ecespede <eduard.cespedes@dsquare.be>
2015-01-16 19:09:19 +0100

    second initial commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name theatreStatisticsApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the theatreStatisticsApp
+ */
+angular.module('theatreStatisticsApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name theatreStatisticsApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the theatreStatisticsApp
+ */
+angular.module('theatreStatisticsApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
