701092a9bfbc9769152e78fbf74ff83a01d4b431
ecespede <eduard.cespedes@dsquare.be>
2015-01-21 07:44:05 +0100

    Adding lodash and keep advancing with D3

--- app/scripts/directives/chartdirective.js
+++ app/scripts/directives/chartdirective.js
@@ -9,10 +9,17 @@
 angular.module('theatreStatisticsApp')
   .directive('d3Chart', function () {
     function link(scope, element, attrs) {
-      var dataset = [5, 10, 15, 20, 25];
+      //var dataset = [5, 10, 15, 20, 25];
+      var dataset = _.map(_.range(30), function(i){
+         return Math.random() * 50;
+      });
       var multiplier = 8;
       var width = 400;
       var height = 300;
+
+      var yScale = d3.scale.linear()
+        .domain([0, d3.max(dataset) * 1.1])
+        .range([0, height]);
 
       var svg = d3.select(element[0])
         .append('svg')
@@ -28,11 +35,11 @@ angular.module('theatreStatisticsApp')
           return i * 22;
         })
         .attr('y', function (d) {
-          return 300 - d * multiplier;
+          return height - yScale(d);
         })
         .attr('width', 20)
         .attr('height', function (d) {
-          return d * multiplier;
+          return yScale(d);
         });
     }
 
